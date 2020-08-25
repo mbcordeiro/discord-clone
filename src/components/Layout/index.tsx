83ac454e67364a2d1580821e83a4408e799a362a
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-08-25 13:04:47 -0300

    Chanel info component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,12 +4,14 @@ import { Grid } from "./styles";
 
 import ServerList from "../ServerList";
 import ServerName from "../ServerName";
+import ChanelInfo from "../ChanelInfo";
 
 const Layout: React.FC = () => {
   return (
     <Grid>
       <ServerList />
       <ServerName />
+      <ChanelInfo />
     </Grid>
   );
 };
